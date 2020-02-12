b1e97d8fade61b062947f15d1437afb1c4d771ae
peersmg <peersmg@gmail.com>
2020-02-12 23:20:52 +0000

    [Matt] Inital commit

---
+++ src/main/java/com/matt/mapgenerator/MapGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.matt.mapgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matt/mapgenerator/MapGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matt.mapgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
